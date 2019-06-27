123cecdf57b08f1cb0cddb0825a5b667ebb031a5
olegphpgithub <olegphp@yandex.ru>
2019-06-27 12:14:31 +0300

    Add CmdLineParser.cpp file;

--- WAVUtils/main.cpp
+++ WAVUtils/main.cpp
@@ -4,6 +4,7 @@
 #include <tchar.h>
 
 #include "WAVUtils.h"
+#include "CmdLineParser.h"
 
 
 int isFileExists(const char *filename) {
@@ -19,18 +20,32 @@ int isFileExists(const char *filename) {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	if(argc != 5) {
-		printf("Usage: prog.exe import/export <WAVFile> <PayloadFile> <Key>");
+
+	CCmdLineParser realParser(::GetCommandLine());
+
+    bool ok = false;
+    do {
+        if ( (!realParser.HasKey(TEXT("ACTION")))
+            || (!realParser.HasKey(TEXT("WAV")))
+			|| (!realParser.HasKey(TEXT("PAYLOAD")))
+            || (!realParser.HasKey(TEXT("KEY"))) ) {
+                break;
+        }
+        ok = true;
+    } while(false);
+    
+    if(!ok) {
+		printf("Usage: prog.exe /ACTION=import/export /WAV=<WAVFile> /PAYLOAD=<PayloadFile> /KEY=<Key>");
 		return 1;
-	}
+    }
 
 	char payloadFilePath[MAX_PATH];
 	ZeroMemory(payloadFilePath, MAX_PATH);
-	strcpy_s(payloadFilePath, MAX_PATH, argv[3]);
+	strcpy_s(payloadFilePath, MAX_PATH, realParser.GetVal(_T("PAYLOAD")));
 
-	if(_strcmpi(argv[1], "import") == 0) {
+	if(_strcmpi(realParser.GetVal(_T("ACTION")), "import") == 0) {
 
-		WAVUtils wav(argv[2], argv[4]);
+		WAVUtils wav(realParser.GetVal(_T("WAV")), realParser.GetVal(_T("KEY")));
 
 		if(!isFileExists(payloadFilePath)) {
 			printf("PayloadFile does not exist.\n");
@@ -44,8 +59,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		/** Read to file */
 		
-		//WAVUtils wav(argv[2], argv[4]);
-		//wav.ReadTrackToFile(0, payloadFilePath);
+		WAVUtils wav(realParser.GetVal(_T("WAV")), realParser.GetVal(_T("KEY")));
+		wav.ReadTrackToFile(0, payloadFilePath);
 
 		/** Read to memory */
 		
@@ -54,9 +69,10 @@ int _tmain(int argc, _TCHAR* argv[])
 		//wav.ReadTrackToMemory(0, &p, &dw);
 
 		/** Read from memory to memory */
-
+		
+		/*
 		FILE *f;
-		errno_t err = fopen_s(&f, argv[2], "r");
+		errno_t err = fopen_s(&f, realParser.GetVal(_T("WAV")), "r");
 		DWORD dv = GetLastError();
 		fseek(f, 0, SEEK_END);
 		long wavFileSize = ftell(f);
@@ -65,11 +81,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		fread(buffer, 1, wavFileSize, f);
 		fclose(f);
 		
-		WAVUtils wav2(buffer, (size_t)wavFileSize, argv[4]);
+		WAVUtils wav2(buffer, (size_t)wavFileSize, realParser.GetVal(_T("KEY")));
 		unsigned char *pointer;
 		DWORD size;
 		wav2.ReadTrackToMemory(0, &pointer, &size);
-		
+		*/
+
 	}
 	
 	
